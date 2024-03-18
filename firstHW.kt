0a7244537cf8b437a4d3d267c8a856d229dd60fd
Dahee33323 <163131270+Dahee33323@users.noreply.github.com>
2024-03-18 19:35:07 +0900

    Update firstHW.kt

--- firstHW.kt
+++ firstHW.kt
@@ -50,11 +50,38 @@ fun main() {
         70 -> 'C'
         60 -> 'D'
         else -> 'F'
-    } // null로 선언해도 사용 안하면 redundant 에러뜸
+    } 
     println("**************************성적표**************************")
     println("학번\t국어\t영어\t총점\t평균\t학점\t등수")
     println("---------------------------------------------------------")
     println("$no\t$kor\t$eng\t$tot\t$avg\t$point\t$rank")
 
+}
+
+// 3 
+fun main() {
+    val no= 1
+    val kor : Int
+    kor = 78
+    val eng = 90;
+    val total = kor.plus(eng)
+    val avg = total.div(2)
+    val grade = 'A'
+    
+    val rank : Char
+    
+    rank = if (avg >= 90) 'A'
+    else if (avg >= 80) 'B'
+    else if (avg >= 70) 'C'
+    else if (avg >= 60) 'D'
+    else 'E'
+    
+    val message = """*************************성적표*************************
+  					|학번	국어	영어	총점	평균	학점	등수
+  					|$no	$kor	$eng	$total	$avg	$grade	$rank""".trimMargin()
+    
+    println(message+"\n")
+    val comment = """trimMargin의 기본 문자열은 '|', 이걸 기준으로 공백이 제거됨"""
+    println(comment)
 }
 
